428b32310270c30c62728c837f207ae7e397b490
Jaime <33208428+jaimearango12@users.noreply.github.com>
2022-02-21 07:13:42 -0500

    primer archivo modificado de ts

--- JS profesional/Platzi video/assets/index.js
+++ JS profesional/Platzi video/assets/index.js
@@ -20,3 +20,9 @@ muteButton.onclick = () => {
     player.mute();
   }
 };
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
+}
---
+++ JS profesional/typescript/index.ts
@@ -0,0 +1,40 @@
+// Boolean
+let muted: boolean = true;
+muted = false;
+
+// Números
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = 'Richard';
+let saludo = `Me llamo ${nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ['Isabel', 'Nicole', 'Raul'];
+// people.push("9000")
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push('Ricardo');
+peopleAndNumbers.push(9001);
+
+// Enum
+enum Color {
+  Rojo = 'Rojo',
+  Verde = 'Verde',
+  Azul = 'Azul',
+  Amarillo = 'Amarillo',
+}
+
+let colorFavorito: Color = Color.Amarillo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = 'Joker';
+comodin = { type: 'Wildcard' };
+
+// Object
+let someObject: object = { type: 'Wildcard' };
