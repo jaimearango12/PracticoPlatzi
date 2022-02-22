118a52fc47bd13dc484cd7edacac7b2d0faf3cb1
Jaime <33208428+jaimearango12@users.noreply.github.com>
2022-02-21 22:13:22 -0500

    se convirtio en version final Ts

--- JS profesional/Platzi video/assets/index.ts
+++ JS profesional/Platzi video/assets/index.ts
@@ -1,7 +1,7 @@
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+import AutoPause from './plugins/AutoPause';
 
-import MediaPlayer from './mediaPlayer.js';
-import AutoPlay from './plugins/AutoPlay.js';
-import AutoPause from './plugins/AutoPause.ts';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
@@ -9,10 +9,10 @@ const player = new MediaPlayer({
   plugins: [new AutoPlay(), new AutoPause()],
 });
 
-const playButton = document.querySelector('#playButton');
+const playButton: HTMLElement = document.querySelector('#playButton');
 playButton.onclick = () => player.togglePlay();
 
-const muteButton = document.querySelector('#muteButton');
+const muteButton: HTMLElement = document.querySelector('#muteButton');
 muteButton.onclick = () => {
   if (player.media.muted) {
     player.unmute();
