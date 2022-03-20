1800af254f7051ee8a47990e59cc55755a2c58c8
Jaime <33208428+jaimearango12@users.noreply.github.com>
2022-03-19 21:00:27 -0500

    hasta aqui fue webpack
    desde el siguiente commit voy a aprender webpack integrado con react

--- webpack curso/src/index.js
+++ webpack curso/src/index.js
@@ -1,6 +1,8 @@
-import sum from './utils/sum.js'
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl';
 
-const hello = 'hello';
-
-console.log(hellonpox);
-console.log(sum(5+3));
+(async function App() {
+  const main = null || document.getElementById('main');
+  main.innerHTML = await Template();
+})();
---
+++ webpack curso/webpack.config.dev.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPLugin = require('mini-css-extract-plugin'); 
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv= require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'), //indica donde se encuentra el directorio y donde se va a usar
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
+    },
+    mode: 'development',
+    devtool:'source-map',
+    watch: true,
+    resolve: {
+        extensions : ['.js'],
+        alias:{
+            '@utils': path.resolve(__dirname,'src/utils/'),
+            '@templates': path.resolve(__dirname,'src/templates/'),
+            '@styles': path.resolve(__dirname,'src/styles/'),
+            '@images': path.resolve(__dirname,'src/assets/images/'),
+        }
+    },
+    module: {
+        rules: [
+        {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use:{
+                loader: 'babel-loader'
+            }
+        },
+        {
+            test: /\.css|.styl$/i,
+            use:[
+                MiniCssExtractPLugin.loader,
+                'css-loader',
+                'stylus-loader'
+            ],
+        },
+        {
+            test: /\.png/,
+            type: 'asset/resource'
+        },
+        {
+            test : /\.(woff|woff2)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff",
+                    name: "[name].[contenthash].[ext]",
+                    outputPath: "./assets/fonts/",
+                    publicPath: "../assets/fonts/",
+                    esModule: false,
+                }
+            }
+        }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject:true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPLugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns:[{
+                from:path.resolve(__dirname, "src", "assets/images"),
+                to: "assets/images"
+            }]
+        }),
+        new Dotenv(),
+        new BundleAnalyzerPlugin(),
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback:true,
+        port: 8080,
+        open:true,
+    }
+    
+}
