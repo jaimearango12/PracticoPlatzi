b50b735ec61ce9fee77595bcaf9f6d00bd1e2661
Jaime <33208428+jaimearango12@users.noreply.github.com>
2022-03-29 20:33:10 -0500

    inicio con el paquete Angular Forms
    hasta aqui va el interpolacion, property binding y el event binding

--- fundamentos angular/my-store/src/app/app.component.ts
+++ fundamentos angular/my-store/src/app/app.component.ts
@@ -9,4 +9,28 @@ export class AppComponent {
   name = 'Jaime';
   age = 12;
   img = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+  person = {
+    nombre: 'jaime',
+    age: 12,
+    avatar:  'https://source.unsplash.com/random'
+  };
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  };
+
+  ageIncrease(){
+    this.person.age += 1;
+  };
+
+  onScroll(event:Event){
+    const element= event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  ChangeName(event:Event){
+    const element = event.target as HTMLInputElement;
+    this.person.nombre = element.value;
+  }
 }
